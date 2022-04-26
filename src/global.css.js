faa9f38ecdaadb0f6ff6529ebe90bdd3ff6c6c65
Pius Prince <45078705+piusprince@users.noreply.github.com>
2022-04-26 15:22:32 +0000

    Create app skeleton (#1) (#1)
    * Add linebreak-style to override linting errors
    
    * Add react router dom and styled-components
    
    * Initialize react router and override default page style
    
    * Create global.css.js
    
    Global styling file with styled components
    
    * Update yarn.lock

---
+++ src/global.css.js
@@ -0,0 +1,11 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+@import url('https://fonts.googleapis.com/css2?family=Bitter:wght@200;300;400;700&display=swap');
+    body {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+        font-family: 'Bitter', Monteserrat;
+    }
+`;
