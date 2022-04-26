60f1d2db5f252a3c7854701cff339bf66958eca2
piusprince <45078705+piusprince@users.noreply.github.com>
2022-04-26 12:52:20 +0000

    Create Header component

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Header = () => {
+    return (
+        <Wrapper>
+            Header Placeholder
+        </Wrapper>
+    )
+}
+
+export default Header
+
+const Wrapper = styled.header``
