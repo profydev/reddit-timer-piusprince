11ceeccb0a1edc34ca3aeec17e191f7951d7ffae
piusprince <45078705+piusprince@users.noreply.github.com>
2022-04-26 12:33:42 +0000

    Initialize react router and override default page style

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import GlobalCSS from './global.css';
 
 function App() {
   return (
     <div>
+      <GlobalCSS />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<h1>Home</h1>} />
+          <Route path="/search" element={<h1>Search</h1>} />
+        </Routes>
+      </BrowserRouter>
       App Placeholder
     </div>
   );
